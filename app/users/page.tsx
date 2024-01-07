9ac030f0471feccf9cf6e16674ba9aa7431a548e
progwired <progwired@gmail.com>
2024-01-08 02:52:25 +0530

    adding users list and updating it

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,10 +5,13 @@ const UsersPage = async () => {
   const users = await prisma.user.findMany();
   return (
     <div>
-      <h1 className="text-1xl">Users page</h1>
+      <h1 className="text-2xl border-b pb-1 mb-2">Users page</h1>
       <ul>
         {users.map((u) => (
-          <li key={u.name}>{u.name}</li>
+          <li key={u.name} className="flex">
+            <strong>{u.name} - </strong>
+            <span>{u.email}</span>
+          </li>
         ))}
       </ul>
     </div>

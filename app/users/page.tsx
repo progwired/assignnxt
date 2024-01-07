780d978f2a26786fac67f7dcdd3aecf7fc8c98b7
progwired <progwired@gmail.com>
2024-01-08 02:45:41 +0530

    adding users list

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import prisma from "../../lib/prisma";
 
-const UsersPage = () => {
-  return <div>Hello this is users page</div>;
+const UsersPage = async () => {
+  const users = await prisma.user.findMany();
+  return (
+    <div>
+      <h1 className="text-1xl">Users page</h1>
+      <ul>
+        {users.map((u) => (
+          <li key={u.name}>{u.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default UsersPage;

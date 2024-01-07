03652207e32dd3ce8b2eee4e4cb8c090f2ac5704
progwired <progwired@gmail.com>
2024-01-08 04:58:59 +0530

    Updating issues page.

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import prisma from "../../lib/prisma";
 
-const IssuesPage = () => {
-  return <div>Hello! How many issues are there?</div>;
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
+  return (
+    <div>
+      <h1>Issues Page: </h1>
+      <div className="flex flex-col">
+        {issues.map((each) => (
+          <div key={each.id} className="border-b py-4">
+            <h2 className="text-2xl">{each.title}</h2>
+            <p>{each.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default IssuesPage;

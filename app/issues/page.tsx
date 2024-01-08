165ff9bb39b5563c1492bfbaf0cb865e48e26a99
progwired <progwired@gmail.com>
2024-01-08 16:59:11 +0530

    adding radix-ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import prisma from "../../lib/prisma";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
   return (
     <div>
       <h1>Issues Page: </h1>
-      <div className="flex flex-col">
+      <div className="flex flex-col mb-4">
         {issues.map((each) => (
           <div key={each.id} className="border-b py-4">
             <h2 className="text-2xl">{each.title}</h2>
@@ -14,6 +15,7 @@ const IssuesPage = async () => {
           </div>
         ))}
       </div>
+      <Button>Add new issue</Button>
     </div>
   );
 };

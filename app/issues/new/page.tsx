62e875b6ec2aa78a75316db7b637e231b0061be1
progwired <progwired@gmail.com>
2024-01-16 01:23:01 +0530

    adding mdx

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,26 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
+import { MdEditor } from "md-editor-rt";
+import "md-editor-rt/lib/style.css";
+// import SimpleMDE from "react-simplemde-editor";
+// import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
+  const [issue, setIssue] = useState("");
   return (
     <div className="space-y-3">
       <TextField.Root>
         <TextField.Input placeholder="Title" />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+      {/* <SimpleMDE placeholder="Description" /> */}
+      {/* <TextArea placeholder="Description"></TextArea> */}
+      <MdEditor
+        modelValue={issue}
+        onChange={setIssue}
+        language="en-US"
+        placeholder="Description"
+      ></MdEditor>
       <Button>Submit New Issue</Button>
     </div>
   );

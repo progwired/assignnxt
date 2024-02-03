e786da2e04c87cfa3e0e991076244673839549aa
progwired <progwired@gmail.com>
2024-02-04 05:28:05 +0530

    adding color to sidenav

--- app/components/Sidenav.tsx
+++ app/components/Sidenav.tsx
@@ -5,32 +5,30 @@ import Link from "next/link";
 function Sidenav() {
   return (
     <div>
-      <ul>
-        {sidenavlist_js.map((each) => (
-          <li key={each.id}>
-            <Link
-              href={each.href}
-              className="px-4 py-2 flex border-b border-r uppercase"
-            >
-              {each.label}
-            </Link>
-            {each.data && (
-              <ul>
-                {each.data.map((t) => (
-                  <li key={t.id}>
-                    <Link
-                      href={t.href}
-                      className="px-4 py-2 flex border-b border-r capitalize"
-                    >
-                      {t.label}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </ul>
+      {sidenavlist_js.map((each) => (
+        <div key={each.id}>
+          <Link
+            href={each.href}
+            className="px-4 py-2 flex border-b border-r uppercase"
+          >
+            {each.label}
+          </Link>
+          {each.data && (
+            <ul className="ml-4">
+              {each.data.map((t) => (
+                <li key={t.id}>
+                  <Link
+                    href={t.href}
+                    className="px-4 py-2 flex border-b border-r capitalize hover:bg-slate-200"
+                  >
+                    {t.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
